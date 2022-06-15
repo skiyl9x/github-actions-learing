2da3ae950e7bccf0c82c2dfbdd14026aed9ebb75
Alexander Shidlovskyi <79592299+skiyl9x@users.noreply.github.com>
2022-06-15 17:30:56 +0300

    Delete runners/redhat8 directory

--- runners/redhat8/start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-ACCESS_TOKEN=$ACCESS_TOKEN
-REG_TOKEN=$(curl -sX POST -H "Authorization: token $ACCESS_TOKEN" https://api.github.com/repos/skiyl9x/github-actions-learing/actions/runners/registration-token | jq .token --raw-output)
-cd /home/runner/actions-runner
-
-./config.sh --url https://github.com/skiyl9x/github-actions-learing --token $REG_TOKEN --labels RedHat8,terraform-instance
-./run.sh & tail -f /dev/null
-
