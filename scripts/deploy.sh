d49c5d0981df6322ab74cecd07aff91735fd0a35
skiyl9x <skiyl9x@gmail.com>
2022-06-21 12:10:32 +0300

    add bash script for deploying application

---
+++ scripts/deploy.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+$DOCKER_REMOTE_IP=${1}
+TOKEN=${2}
+
+echo "IMAGE_NAME=ghcr.io/skiyl9x/github-actions-learing:dev" > ../deploy/.env
+echo  $TOKEN   |  docker login ghcr.io -u skiyl9x --password-stdin
+docker-compose --host tcp://$DOCKER_REMOTE_IP:2375 --tlsverify --tlscacert "ca.pem" --tlscert "client-cert.pem" --tlskey "client-key.pem" -f ../deploy/docker-compose.yml up -d
