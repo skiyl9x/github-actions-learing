7453140befa86e1a577aeca7704d4c88e0fe9957
Alexander Shidlovskyi <79592299+skiyl9x@users.noreply.github.com>
2022-06-21 20:01:04 +0300

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 PRIVATE_SSH_KEY="${1}"
-USER=${2}
-DOCKER_REMOTE_IP=${3}
-REGISTRY_URL=${4}
-IMAGE_NAME=${5}
-TAG=${6}
+USER="${2}"
+DOCKER_REMOTE_IP="${3}"
+REGISTRY_URL="${4}"
+IMAGE_NAME="${5}"
+TAG="${6}"
 
 echo "* Creating .env file for docker-compose"
 echo "IMAGE = \"$REGISTRY_URL/$IMAGE_NAME:$TAG\"" > ../deploy/.env
