bda63bc152e9b69c77a15fdb34a286f3bc8eb5c1
skiyl9x <skiyl9x@gmail.com>
2022-06-21 14:37:23 +0300

    add clear images, fix

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -10,7 +10,7 @@ echo "IMAGE_NAME = \"ghcr.io/skiyl9x/github-actions-learing:dev\"" > ../deploy/.
 docker context create my-context --description "some description" --docker "host=tcp://$DOCKER_REMOTE_IP:2375,ca=./ca.pem,cert=./client-cert.pem,key=./client-key.pem"
 docker context use my-context
 echo  $TOKEN   |  docker login ghcr.io -u skiyl9x --password-stdin
-docker-compose down
-docker-compose rm --all
+docker-compose -f ../deploy/docker-compose.yml down
+docker-compose -f ../deploy/docker-compose.yml rm --all
 docker-compose -f ../deploy/docker-compose.yml  up -d 
 cat ../deploy/.env
