800ef70a7a522973135293547c14d5229709a959
skiyl9x <skiyl9x@gmail.com>
2022-06-21 17:54:13 +0300

    change deploy.sh script with logs

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,9 +6,16 @@ REGISTRY_URL=${4}
 IMAGE_NAME=${5}
 TAG=${6}
 
+echo "* Creating .env file for docker-compose"
 echo "IMAGE = \"$REGISTRY_URL/$IMAGE_NAME:$TAG\"" > ../deploy/.env
+
+echo "* Adding SSH private key"
 mkdir -p ~/.ssh
 echo "$PRIVATE_SSH_KEY" > ~/.ssh/id_rsa
 chmod 600 ~/.ssh/id_rsa
+
+echo "* Deploying container with docker-compose"
 docker-compose -H "ssh://$USER@$DOCKER_REMOTE_IP" -f ../deploy/docker-compose.yml up -d
+
+echo "* Deleting SSH private key"
 rm -f ~/.ssh/id_rsa
