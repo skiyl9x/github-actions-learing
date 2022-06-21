794c56941fb8cd28f1682fc7cd44c570da217029
skiyl9x <skiyl9x@gmail.com>
2022-06-21 16:41:36 +0300

    change method to deploy application, using ssh, fixing 3

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -24,4 +24,4 @@ TAG=${6}
 whoami
 echo "IMAGE = \"$REGISTRY_URL/$IMAGE_NAME:$TAG\"" > ../deploy/.env
 echo "$PRIVATE_SSH_KEY" > ~/.ssh/id_rsa
-docker-compose -H ssh://$USER@$DOCKER_REMOTE_IP -f ../deploy/docker-compose.yml up -d
+docker-compose -H "ssh://$USER@$DOCKER_REMOTE_IP" -f ../deploy/docker-compose.yml up -d
