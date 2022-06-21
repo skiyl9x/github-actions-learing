99ffb4001aeb2313aec60d5018521518eb955d18
skiyl9x <skiyl9x@gmail.com>
2022-06-21 13:44:26 +0300

    fix docker-compose format

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -9,4 +9,4 @@ echo "IMAGE_NAME=ghcr.io/skiyl9x/github-actions-learing:dev" > ../deploy/.env
 
 docker context create my-context --description "some description" --docker "host=tcp://$DOCKER_REMOTE_IP:2375,ca=./ca.pem,cert=./client-cert.pem,key=./client-key.pem"
 docker context use my-context
-docker-compose up -f ../deploy/docker-compose.yml -d
+docker-compose -f ../deploy/docker-compose.yml  up -d
